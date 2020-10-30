c1fd6799c6ee78cd8aa11a80ffeb4b3a8f66a5b2
Alexey Petrakov <aapetra@yandex.ru>
2020-10-30 16:28:08 +0300

    update

---
+++ CorsairsTrading/CorsairsTrading/SqlConnect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorsairsTrading
+{
+    public partial class MainForm : Form
+    {
+        public static void ConnectSQL()
+        {
+            SqlConnection conn;
+            //Data Source=COMPUTER\SQLEXPRESS;Initial Catalog=CorsairsStores;User ID=sa;Password=***********
+            using (conn = new SqlConnection(@"Data Source=COMPUTER\SQLEXPRESS;Initial Catalog=CorsairsStores;User ID=sa;Password=1qA-2wS-3eD"))
+            {
+                try
+                {
+                    conn.Open();
+                    MessageBox.Show("Соединено");
+                    conn.Close();
+                }
+                catch (Exception help)
+                {
+                    MessageBox.Show(help.Message);
+                }
+            }
+        }
+    }
+}
